f5e14a5eff68cf46eb6ae11cc6cefc5015fab4dd
Rohan Pandit <rohananandpandit@gmail.com>
2023-02-06 08:34:00 +0000

    Add multiple tabs in query editor
    Include placeholder for basic query template

--- frontend/src/components/query-editor/QueryEditor.tsx
+++ frontend/src/components/query-editor/QueryEditor.tsx
@@ -1,13 +1,85 @@
-import React from "react";
-import { Input } from 'antd';
-import { observer } from "mobx-react-lite";
+import React, { useRef, useState } from "react";
+import { Tabs, Input } from "antd";
 
 const { TextArea } = Input;
 
-const QueryEditor: React.FC = observer(() => (
-  <>
-    <TextArea rows={10} allowClear/>
-  </>
-));
+type TargetKey = React.MouseEvent | React.KeyboardEvent | string;
+
+const queryTemplate = 
+`\
+SELECT ?s ?p ?o
+WHERE { ?s ?p ? o}        
+`;
+
+const initialItems = [
+  {
+    label: "Query 1",
+    children: <TextArea rows={10} allowClear placeholder={queryTemplate} />,
+    key: "1",
+  },
+];
+
+const QueryEditor: React.FC = () => {
+  const [activeKey, setActiveKey] = useState(initialItems[0].key);
+  const [items, setItems] = useState(initialItems);
+  const newTabIndex = useRef(initialItems.length);
+
+  const onChange = (newActiveKey: string) => {
+    setActiveKey(newActiveKey);
+  };
+
+  const add = () => {
+    const newActiveKey = `newTab${newTabIndex.current++}`;
+    const newPanes = [...items];
+    newPanes.push({
+      label: `Query ${newTabIndex.current}`,
+      children: <TextArea rows={10} allowClear placeholder={queryTemplate} />,
+      key: newActiveKey,
+    });
+    setItems(newPanes);
+    setActiveKey(newActiveKey);
+  };
+
+  const remove = (targetKey: TargetKey) => {
+    let newActiveKey = activeKey;
+    let lastIndex = -1;
+    items.forEach((item, i) => {
+      if (item.key === targetKey) {
+        lastIndex = i - 1;
+      }
+    });
+    const newPanes = items.filter((item) => item.key !== targetKey);
+    if (newPanes.length && newActiveKey === targetKey) {
+      if (lastIndex >= 0) {
+        newActiveKey = newPanes[lastIndex].key;
+      } else {
+        newActiveKey = newPanes[0].key;
+      }
+    }
+    setItems(newPanes);
+    setActiveKey(newActiveKey);
+  };
+
+  const onEdit = (
+    targetKey: React.MouseEvent | React.KeyboardEvent | string,
+    action: "add" | "remove"
+  ) => {
+    if (action === "add") {
+      add();
+    } else {
+      remove(targetKey);
+    }
+  };
+
+  return (
+    <Tabs
+      type="editable-card"
+      onChange={onChange}
+      activeKey={activeKey}
+      onEdit={onEdit}
+      items={items}
+    />
+  );
+};
 
 export default QueryEditor;
