e662700fa5df7df3ba0fcb9f4a1610a4dc8dc029
Rohan Pandit <rohananandpandit@gmail.com>
2023-02-11 23:55:04 +0000

    Rename visualise-results to graph-visualisation

--- frontend/src/components/graph-visualisation/GraphVisualisation.tsx
+++ frontend/src/components/graph-visualisation/GraphVisualisation.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 import Demo from "./Demo";
 
-const VisualiseResults = observer(() => {
+const GraphVisualisation = observer(() => {
   return (
     <div style={{ width: "calc(100vw - 200)", height: "82vh" }}>
       {/* <div style={{ width: '100%', height: '100%'}}> */}
@@ -10,4 +10,4 @@ const VisualiseResults = observer(() => {
   );
 });
 
-export default VisualiseResults;
+export default GraphVisualisation;
