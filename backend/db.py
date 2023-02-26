338b18851ee0a2e4729b711bfbdfe8b5ac6ffd40
Rohan Pandit <rohananandpandit@gmail.com>
2023-02-26 08:57:11 +0000

    Return query history in descending order by date

--- backend/db.py
+++ backend/db.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 conn = sqlite3.connect('./database.db')
 conn.row_factory = sqlite3.Row
-conn.execute('DROP TABLE query')
+# conn.execute('DROP TABLE query')
 conn.execute('CREATE TABLE IF NOT EXISTS query ('
              'id INTEGER PRIMARY KEY,'
              'sparql TEXT,'
@@ -16,7 +16,8 @@ def get_queries(repository_id: str):
     conn = sqlite3.connect('./database.db')
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-    cur.execute('SELECT * FROM query WHERE repositoryId = ?', [repository_id])
+    cur.execute('SELECT * FROM query WHERE repositoryId = ? ORDER BY date DESC',
+                [repository_id])
     conn.commit()
     return [dict(row) for row in cur.fetchall()]
 
