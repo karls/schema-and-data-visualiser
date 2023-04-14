ab28b3e48efdbf03d25590647525d14833db4513
Rohan Pandit <rohananandpandit@gmail.com>
2023-04-14 22:23:29 +0100

    Check if string is a URI before removing prefix

--- frontend/src/utils/queryResults.ts
+++ frontend/src/utils/queryResults.ts
@@ -10,10 +10,15 @@ export function isEmpty(results: QueryResults) {
   return results.data.length === 0;
 }
 
-export function removePrefix(uri: string): string {
-  uri = uri.slice(1, -1);
+export function isURL(text: string) {
+  const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+  return text.match(URL_REGEX);
+}
+export function removePrefix(text: string): string {
+  if (!isURL(text)) return text;
+  const uri = text;
   const tokens = uri.split("/").filter((t) => t !== "");
   if (tokens.length === 0) return "";
   const name = tokens.at(-1);
-  return name!.split("#").at(-1) ?? "";
+  return (name!.split("#").at(-1) ?? "").replaceAll('+', ' ');
 }
