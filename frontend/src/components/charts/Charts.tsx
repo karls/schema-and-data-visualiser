2268eacbbe9e92dd1240cc089e79bd3196e74bd3
Rohan Pandit <rohananandpandit@gmail.com>
2023-04-14 22:31:31 +0100

    Add tab for bar chart in Charts component

--- frontend/src/components/charts/Charts.tsx
+++ frontend/src/components/charts/Charts.tsx
@@ -1,5 +1,29 @@
-const Charts = () => {
-  return <>Charts</>;
+import { Tabs, TabsProps } from "antd";
+import { QueryResults } from "../../types";
+import BarGraph from "./BarGraph";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../stores/store";
+
+type ChartsProps = {
+  results: QueryResults;
 };
 
-export default Charts;
+const Charts = ({ results }: ChartsProps) => {
+  const { settings } = useStore();
+  const items: TabsProps["items"] = [
+    {
+      key: "1",
+      label: `Bar Chart`,
+      children: (
+        <BarGraph
+          results={results}
+          width={Math.floor((window.screen.width - settings.sidebarWidth) * 0.75)}
+          height={400}
+        />
+      ),
+    },
+  ];
+  return <Tabs defaultActiveKey="1" items={items} />;
+};
+
+export default observer(Charts);
