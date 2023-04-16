5589dbbc850f8d32accae98af97fbe42a71ee65f
Rohan Pandit <rohananandpandit@gmail.com>
2023-04-16 08:07:01 +0100

    Track label and text of currently open queries
    Store the label and text of queries persistently to maintain state on refreshing page

--- frontend/src/stores/queries-store.ts
+++ frontend/src/stores/queries-store.ts
@@ -1,25 +1,29 @@
 import { makeAutoObservable } from "mobx";
-import { QueryRecord } from "../types";
 import { makePersistable } from "mobx-persist-store";
 import RootStore from "./root-store";
 
 type QueriesState = {
-  queryHistory: QueryRecord[];
-  openQueries: string[];
+  totalQueries: number;
+  openQueries: { [key: string]: { label: string; text: string } };
 };
 
 class QueriesStore {
   rootStore: RootStore;
   state: QueriesState = {
-    queryHistory: [],
-    openQueries: [],
+    totalQueries: 1,
+    openQueries: {
+      "1": {
+        label: "Query 1",
+        text: "",
+      },
+    },
   };
 
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
     makeAutoObservable(this);
     makePersistable(this, {
-      name: "Settings",
+      name: "Queries",
       properties: [
         {
           key: "state",
@@ -33,6 +37,26 @@ class QueriesStore {
 
   setState(state: QueriesState) {
     this.state = state;
+  }
+
+  getOpenQueries() {
+    return this.state.openQueries;
+  }
+
+  setQueryText(id: string, text: string) {
+    this.state.openQueries[id].text = text;
+  }
+
+  addQuery(qid: string) {
+    this.state.totalQueries++;
+    this.state.openQueries[qid] = {
+      label: `Query ${qid}`,
+      text: "",
+    };
+  }
+
+  getTotalQueries(): number {
+    return this.state.totalQueries;
   }
 }
 
