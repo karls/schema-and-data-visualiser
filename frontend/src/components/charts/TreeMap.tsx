d002f13674ad0a9179277d45ca5e765b0a9743f7
Rohan Pandit <rohananandpandit@gmail.com>
2023-04-23 11:10:18 +0100

    Add custom Tooltip to show name in Treemap

--- frontend/src/components/charts/TreeMap.tsx
+++ frontend/src/components/charts/TreeMap.tsx
@@ -1,6 +1,8 @@
-import { Tooltip, Treemap } from "recharts";
+import { Cell, Tooltip, Treemap } from "recharts";
 import { QueryResults } from "../../types";
 import { removePrefix } from "../../utils/queryResults";
+import { useStore } from "../../stores/store";
+import randomColour from "randomcolor";
 
 type TreeMapProps = {
   results: QueryResults;
@@ -15,14 +17,38 @@ export const TreeMap = ({
   width,
   height,
 }: TreeMapProps) => {
+  const rootStore = useStore();
+  const settings = rootStore.settingsStore;
+
   const data = [
     {
       name: results.header[columnIndex],
       children: results.data.map((row) => {
-        return { name: removePrefix(row[0]), value: parseInt(row[columnIndex]) };
+        return {
+          name: removePrefix(row[0]),
+          value: parseInt(row[columnIndex]),
+        };
       }),
     },
   ];
+
+  const CustomTooltip = ({ active, payload, label }: any) => {
+    if (active && payload && payload.length) {
+      return (
+        <div
+          style={{
+            backgroundColor: settings.darkMode ? "black" : "#ffff",
+            padding: "5px",
+            border: "columnIndexpx solid #cccc",
+          }}
+        >
+          {`${payload[0].payload.name} : ${payload[0].value}`}
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <Treemap
@@ -34,7 +60,7 @@ export const TreeMap = ({
       stroke="#fff"
       fill="#8884d8"
     >
-      <Tooltip />
+      <Tooltip content={CustomTooltip} />
     </Treemap>
   );
 };
