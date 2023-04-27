05babdf1b9d60ec50151041ff7f2e9c845b92589
Rohan Pandit <rohananandpandit@gmail.com>
2023-04-27 08:13:03 +0100

    Function to call API for clearing query history

--- frontend/src/api/queries.ts
+++ frontend/src/api/queries.ts
@@ -1,16 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 import { QueryRecord } from "../types";
 
-export async function getQueryHistory(repositoryId: string): Promise<QueryRecord[]> {
-    const BACKEND_API = process.env.REACT_APP_BACKEND_API;
-    try {
-      const endpoint = `${BACKEND_API}/query/history?repositoryId=${repositoryId}`;
-      const response = await axios.get(endpoint);
-      const queries = response.data;
-      return queries;
-    } catch (error) {
-        console.log(error);
-    }
-    return [];
+export async function getQueryHistory(
+  repositoryId: string
+): Promise<QueryRecord[]> {
+  const BACKEND_API = process.env.REACT_APP_BACKEND_API;
+  try {
+    const endpoint = `${BACKEND_API}/query/history?repositoryId=${repositoryId}`;
+    const response = await axios.get(endpoint);
+    const queries = response.data;
+    return queries;
+  } catch (error) {
+    console.log(error);
   }
-  
+  return [];
+}
+
+export async function clearQueryHistory(repositoryId: string) {
+  const BACKEND_API = process.env.REACT_APP_BACKEND_API;
+  try {
+    const endpoint = `${BACKEND_API}/query/history/clear?repositoryId=${repositoryId}`;
+    const response = await axios.post(endpoint);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+  return [];
+}
