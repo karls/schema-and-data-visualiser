e21916037ff8ceb01381d0c03685297c3a106518
Rohan Pandit <rohananandpandit@gmail.com>
2023-04-27 08:18:10 +0100

    Add delete button in query history section
    Move QueryHistory component to separate file

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Button, Divider, Dropdown, Popover, Space } from "antd";
+import { Button, Divider, Dropdown, Popover } from "antd";
 import { allRepositories } from "../../api/graphdb";
 import { useStore } from "../../stores/store";
 import { RepositoryInfo } from "../../types";
 import { observer } from "mobx-react-lite";
-import { Typography } from "antd";
+import QueryHistory from "./QueryHistory";
 
-const { Title, Text } = Typography;
 
 const Sidebar = observer(() => {
   const rootStore = useStore();
@@ -50,60 +49,6 @@ const Sidebar = observer(() => {
       </Dropdown>
       <Divider />
       <QueryHistory />
-    </div>
-  );
-});
-
-const QueryHistory = observer(() => {
-  const rootStore = useStore();
-  const repositoryStore = rootStore.repositoryStore;
-
-  useEffect(() => {
-    repositoryStore.updateQueryHistory();
-  }, [repositoryStore]);
-
-  return (
-    <div
-      style={{
-        width: "100%",
-        justifyContent: "center",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Title level={4} style={{ margin: "auto", marginBottom: 5 }}>
-        History
-      </Title>
-      {repositoryStore.getCurrentRepository() === null && (
-        <Text style={{ padding: 5 }}>
-          Select a repository to see the queries you have run in the past
-        </Text>
-      )}
-      {repositoryStore.getCurrentRepository() && repositoryStore.getQueryHistory().length === 0 && (
-        <Text style={{ padding: 5 }}>
-          There are no queries for this repository
-        </Text>
-      )}
-      <Space
-        style={{
-          height: 500,
-          overflow: "auto",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        {repositoryStore.getQueryHistory().map(({ id, sparql, repositoryId, date }) => (
-          <Popover
-            key={`query-${id}`}
-            placement="right"
-            title={`Repository: ${repositoryId}`}
-            content={<div style={{ whiteSpace: "pre-line" }}>{sparql}</div>}
-            trigger="hover"
-          >
-            <Button onClick={() => {}}>{date}</Button>
-          </Popover>
-        ))}
-      </Space>
     </div>
   );
 });
