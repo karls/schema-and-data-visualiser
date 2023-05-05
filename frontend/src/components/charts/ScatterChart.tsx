c499054ff3a885baaca9fe45a8b53f34c3feb118
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-05 08:20:20 +0100

    Use Victory library to display scatter chart
    - Allow user to zoom in chart
    - Display third property using bubble size of points

--- frontend/src/components/charts/ScatterChart.tsx
+++ frontend/src/components/charts/ScatterChart.tsx
@@ -1,22 +1,18 @@
-import {
-  ScatterChart as ScatterRechart,
-  Legend,
-  Tooltip,
-  XAxis,
-  YAxis,
-  Scatter,
-  CartesianGrid,
-  ZAxis,
-  Cell,
-  LabelList,
-  Label,
-  ResponsiveContainer,
-} from "recharts";
 import { QueryResults } from "../../types";
-import { removePrefix } from "../../utils/queryResults";
+import { numericColumns, removePrefix } from "../../utils/queryResults";
 import randomColor from "randomcolor";
 import { useStore } from "../../stores/store";
 import { observer } from "mobx-react-lite";
+import {
+  VictoryAxis,
+  VictoryChart,
+  VictoryLabel,
+  VictoryScatter,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryZoomContainer,
+} from "victory";
+import { useState } from "react";
 
 type ScatterChartProps = {
   results: QueryResults;
@@ -28,48 +24,72 @@ const ScatterChart = observer(
   ({ results, width, height }: ScatterChartProps) => {
     const rootStore = useStore();
     const settings = rootStore.settingsStore;
+    const numIdxs = numericColumns(results);
+    const [col1, setCol1] = useState(numIdxs[0]);
+    const [col2, setCol2] = useState(numIdxs[1]);
+    const [col3, setCol3] = useState(numIdxs[2]);
 
     const data = results.data.map((row) => {
+      const label = `${removePrefix(row[0])}
+
+      ${results.header[col1]}: ${row[col1]}
+      ${results.header[col2]}: ${row[col2]}`;
+
       return {
-        name: removePrefix(row[0]),
-        ...(results.header[1] ? { x: parseInt(row[1]) } : {}),
-        ...(results.header[2] ? { y: parseInt(row[2]) } : {}),
-        ...(results.header[3] ? { z: parseInt(row[3]) } : {}),
+        label,
+        x: parseFloat(row[col1]),
+        y: parseFloat(row[col2]),
+        z: parseFloat(row[col3] ?? 1),
+        fill: randomColor({
+          luminosity: settings.darkMode ? "light" : "dark",
+        }),
+        amount: 2,
       };
     });
 
     return (
-      <ResponsiveContainer width={width || "100%"} height={height}>
-        <ScatterRechart margin={{ top: 35, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid />
-          {results.header[1] && (
-            <XAxis type="number" dataKey="x" name={results.header[1]}>
-              <Label value={results.header[1]} position="bottom" />
-            </XAxis>
-          )}
-          {results.header[2] && (
-            <YAxis type="number" dataKey="y" name={results.header[2]}>
-              <Label value={results.header[2]} angle={-90} position="left" />
-            </YAxis>
-          )}
-          {results.header[3] && (
-            <ZAxis type="number" dataKey="z" name={results.header[3]} />
-          )}
-          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-          <Legend wrapperStyle={{ position: "relative" }} />
-          <Scatter name={results.header[0]} data={data} fill="#8884d8">
-            <LabelList dataKey="name" position="right" />
-            {data.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={randomColor({
-                  luminosity: settings.darkMode ? "light" : "dark",
-                })}
-              />
-            ))}
-          </Scatter>
-        </ScatterRechart>
-      </ResponsiveContainer>
+      <VictoryChart
+        width={width}
+        height={height}
+        theme={VictoryTheme.material}
+        padding={{ bottom: 50, left: 100, right: 10, top: 10 }}
+        domainPadding={{ x: 10, y: 10 }}
+        containerComponent={
+          <VictoryZoomContainer
+            width={width}
+            height={height}
+            responsive={false}
+          />
+        }
+      >
+        <VictoryAxis
+          label={results.header[col1]}
+          axisLabelComponent={<VictoryLabel dy={20}/>}
+          style={{
+            tickLabels: { fontSize: 15, padding: 5 },
+          }}
+          fixLabelOverlap
+          domainPadding={{ x: [10, -10], y: 5 }}
+        />
+        <VictoryAxis
+          label={results.header[col2]}
+          axisLabelComponent={<VictoryLabel dy={-75}/>}
+          style={{
+            tickLabels: { fontSize: 15, padding: 5 },
+          }}
+          dependentAxis
+          fixLabelOverlap
+          domainPadding={{ x: [10, -10], y: 5 }}
+        />
+        <VictoryScatter
+          bubbleProperty="z"
+          maxBubbleSize={10}
+          minBubbleSize={5}
+          labelComponent={<VictoryTooltip style={{ fontSize: 15 }} />}
+          style={{ data: { fill: ({ datum }) => datum.fill } }}
+          data={data}
+        />
+      </VictoryChart>
     );
   }
 );
