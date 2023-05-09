84a2ff5fbb4a67c3f8fee1d96f0af6c8bbabebca
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-09 10:45:54 +0100

    Add repository arg for API functions

--- frontend/src/api/dataset.ts
+++ frontend/src/api/dataset.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
-import { RDFGraph, URI } from "../types";
+import { RDFGraph, RepositoryId, URI } from "../types";
 import { emptyGraph } from "../utils/queryResults";
 
-export async function getClasses(): Promise<URI[]> {
+export async function getClasses(repository: RepositoryId): Promise<URI[]> {
     const BACKEND_API = process.env.REACT_APP_BACKEND_API;
     try {
-      const endpoint = `${BACKEND_API}/dataset/classes`;
+      const endpoint = `${BACKEND_API}/dataset/classes?repository=${repository}`;
       const response = await axios.get(endpoint);
       const repositories = response.data;
       return repositories;
@@ -13,10 +13,10 @@ export async function getClasses(): Promise<URI[]> {
     return [];
 }
 
-export async function getClassHierarchy(): Promise<RDFGraph> {
+export async function getClassHierarchy(repository: RepositoryId): Promise<RDFGraph> {
     const BACKEND_API = process.env.REACT_APP_BACKEND_API;
     try {
-      const endpoint = `${BACKEND_API}/dataset/classes`;
+      const endpoint = `${BACKEND_API}/dataset/classes?repository=${repository}`;
       const response = await axios.get(endpoint);
       const repositories = response.data;
       return repositories;
