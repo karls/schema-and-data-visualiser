fc71bf24f76078388eee55892b3b40495da1121e
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-19 13:59:16 +0100

    Create component for word cloud chart
    Use key column and scalar column as props

--- frontend/src/components/charts/WordCloud.tsx
+++ frontend/src/components/charts/WordCloud.tsx
@@ -1,6 +1,12 @@
 import ReactWordcloud from "react-wordcloud";
 import { QueryResults, Row } from "../../types";
 import { useMemo } from "react";
+import randomColor from "randomcolor";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../stores/store";
+
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
 
 type WordCloudProps = {
   results: QueryResults;
@@ -10,26 +16,59 @@ type WordCloudProps = {
   scalarColumn: string;
 };
 
-export const WordCloud = ({
-  results,
-  width,
-  height,
-  keyColumn,
-  scalarColumn,
-}: WordCloudProps) => {
-  const words: { text: string; value: number }[] = useMemo(() => {
-    const keyIndex = results.header.indexOf(keyColumn);
-    const scalarIndex = results.header.indexOf(scalarColumn);
+export const WordCloud = observer(
+  ({ results, width, height, keyColumn, scalarColumn }: WordCloudProps) => {
+    const rootStore = useStore();
+    const settings = rootStore.settingsStore;
 
-    return results.data.map((row: Row) => {
+    const data: any = useMemo(() => {
+      const keyIndex = results.header.indexOf(keyColumn);
+      const scalarIndex = results.header.indexOf(scalarColumn);
+
+      return results.data.map((row: Row) => {
+        return {
+          text: row[keyIndex] as string,
+          value: parseFloat(row[scalarIndex]),
+        };
+      });
+    }, [keyColumn, results.data, results.header, scalarColumn]);
+
+    const options: any = useMemo(() => {
       return {
-        text: row[keyIndex],
-        value: row[scalarIndex],
+        enableTooltip: true,
+        deterministic: false,
+        fontFamily: "impact",
+        fontStyle: "normal",
+        fontWeight: "normal",
+        fontSize: [60, 60],
+        padding: 0,
+        margin: 0,
+        rotationAngles: [0, 0],
+        rotations: 1,
+        spiral: "rectangular",
+        transitionDuration: 1000,
+        seed: 0,
       };
-    });
-  }, [keyColumn, results.data, results.header, scalarColumn]);
+    }, []);
 
-  return <ReactWordcloud words={words} />;
-};
+    const callbacks = useMemo(() => {
+      return {
+        getWordColor: (word) =>
+          randomColor({ luminosity: settings.darkMode ? "light" : "dark" }),
+      };
+    }, [settings.darkMode]);
+
+    return (
+      <div style={{ display: 'flex', float: "left" }}>
+        <ReactWordcloud
+          words={data}
+          callbacks={callbacks}
+          options={options}
+          minSize={[width, height]}
+        />
+      </div>
+    );
+  }
+);
 
 export default WordCloud;
