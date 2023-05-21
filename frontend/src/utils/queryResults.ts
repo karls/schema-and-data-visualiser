9e47578c78c5636b53dcc34342892aa14641d364
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-21 09:21:11 +0100

    Function to get shades of a hex colour

--- frontend/src/utils/queryResults.ts
+++ frontend/src/utils/queryResults.ts
@@ -77,3 +77,31 @@ export function categoricalColumns(results: QueryResults): number[] {
   }
   return columnIndices;
 }
+
+// export function shadeColor(color: string, percent: number) {
+//   var R: number = parseInt(color.substring(1, 3), 16);
+//   var G = parseInt(color.substring(3, 5), 16);
+//   var B = parseInt(color.substring(5, 7), 16);
+
+//   R = (R * (100 + percent)) / 100;
+//   G = (G * (100 + percent)) / 100;
+//   B = (B * (100 + percent)) / 100;
+
+//   R = R < 255 ? R : 255;
+//   G = G < 255 ? G : 255;
+//   B = B < 255 ? B : 255;
+
+//   R = Math.round(R);
+//   G = Math.round(G);
+//   B = Math.round(B);
+
+//   var RR = R.toString(16).length === 1 ? "0" + R.toString(16) : R.toString(16);
+//   var GG = G.toString(16).length === 1 ? "0" + G.toString(16) : G.toString(16);
+//   var BB = B.toString(16).length === 1 ? "0" + B.toString(16) : B.toString(16);
+//   console.log("#" + RR + GG + BB)
+//   return "#" + RR + GG + BB;
+// }
+
+export function shadeColor(color, amount) {
+  return '#' + color.replace(/^#/, '').replace(/../g, color => ('0'+Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+}
