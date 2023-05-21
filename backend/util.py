b6def2abb6cbb1404a2d224b77dda3a1c174668f
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-21 06:49:49 +0100

    Add function to check if a string is a valid URL
    Fix other issues with parsing or

--- backend/util.py
+++ backend/util.py
@@ -4,6 +4,20 @@ import io
 import shlex
 import re
 
+
+def is_url(text):
+    regex = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|['
+        r'A-Z0-9-]{2,}\.?)|'  # domain... 
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    return re.match(regex, text)
+
+
 def csv_to_json(string):
     reader = csv.DictReader(io.StringIO(string))
     return json.dumps(list(reader))
