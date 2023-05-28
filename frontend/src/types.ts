3ba730d1ddc5414bbf7fef7035fb0b1a4e97d5e5
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-28 20:29:34 +0100

    Add string values in RelationType enum

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -104,8 +104,8 @@ export type QueryAnalysis = {
 };
 
 export enum RelationType {
-  ONE_TO_ONE,
-  ONE_TO_MANY,
-  MANY_TO_ONE,
-  MANY_TO_MANY,
+  ONE_TO_ONE = 'One-to-one',
+  ONE_TO_MANY = 'One-to-many',
+  MANY_TO_ONE = 'Many-to-one',
+  MANY_TO_MANY = 'Many-to-many',
 }
