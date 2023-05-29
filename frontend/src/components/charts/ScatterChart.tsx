ed7e714845d475c2ad4c284a41ed30319cd585e4
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-29 11:49:14 +0100

    Include z scalar column for radius in ScatterChart

--- frontend/src/components/charts/ScatterChart.tsx
+++ frontend/src/components/charts/ScatterChart.tsx
@@ -25,30 +25,34 @@ const ScatterChart = observer(
   ({ results, width, height, variables }: ScatterChartProps) => {
     const rootStore = useStore();
     const settings = rootStore.settingsStore;
-    const [col1, col2] = variables.scalar.map((column) =>
+    const [col1, col2, col3] = variables.scalar.map((column) =>
       results.header.indexOf(column)
     );
-    // ${col3 ? `${results.header[col2]}: ${row[col2]}` : ""}
     const data = useMemo(
       () =>
         results.data.map((row) => {
           const label = `${removePrefix(row[0])}
 
-      ${results.header[col1]}: ${row[col1]}
-      ${results.header[col2]}: ${row[col2]}`;
+      ${results.header[col1]}: ${parseFloat(row[col1]).toLocaleString()}
+      ${results.header[col2]}: ${parseFloat(row[col2]).toLocaleString()}
+      ${
+        col3 !== -1
+          ? `${results.header[col3]}: ${parseFloat(row[col3]).toLocaleString()}`
+          : ""
+      }`;
 
           return {
             label,
             x: parseFloat(row[col1]),
             y: parseFloat(row[col2]),
-            // z: parseFloat(row[col3] ?? 1),
+            z: col3 !== -1 ? parseFloat(row[col3]) : 1,
             fill: randomColor({
               luminosity: settings.darkMode ? "light" : "dark",
             }),
             amount: 2,
           };
         }),
-      [col1, col2, results.data, results.header, settings.darkMode]
+      [col1, col2, col3, results.data, results.header, settings.darkMode]
     );
 
     return (
