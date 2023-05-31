64edc927e6a171da828e2d58b469baeb40137446
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-31 13:08:19 +0100

    Function to get all unique values in a column
    Return key in gropByColumn function

--- frontend/src/utils/queryResults.ts
+++ frontend/src/utils/queryResults.ts
@@ -1,4 +1,4 @@
-import { RDFGraph, QueryResults } from "../types";
+import { RDFGraph, QueryResults, Row } from "../types";
 
 export function isGraph(results: QueryResults) {
   return (
@@ -78,31 +78,7 @@ export function categoricalColumns(results: QueryResults): number[] {
   return columnIndices;
 }
 
-// export function shadeColor(color: string, percent: number) {
-//   var R: number = parseInt(color.substring(1, 3), 16);
-//   var G = parseInt(color.substring(3, 5), 16);
-//   var B = parseInt(color.substring(5, 7), 16);
-
-//   R = (R * (100 + percent)) / 100;
-//   G = (G * (100 + percent)) / 100;
-//   B = (B * (100 + percent)) / 100;
-
-//   R = R < 255 ? R : 255;
-//   G = G < 255 ? G : 255;
-//   B = B < 255 ? B : 255;
-
-//   R = Math.round(R);
-//   G = Math.round(G);
-//   B = Math.round(B);
-
-//   var RR = R.toString(16).length === 1 ? "0" + R.toString(16) : R.toString(16);
-//   var GG = G.toString(16).length === 1 ? "0" + G.toString(16) : G.toString(16);
-//   var BB = B.toString(16).length === 1 ? "0" + B.toString(16) : B.toString(16);
-//   console.log("#" + RR + GG + BB)
-//   return "#" + RR + GG + BB;
-// }
-
-export function shadeColor(color, amount) {
+export function shadeColor(color: string, amount: number) {
   return (
     "#" +
     color
@@ -114,4 +90,25 @@ export function shadeColor(color, amount) {
         ).substr(-2)
       )
   );
+}
+
+export function groupByColumn(
+  data: Row[],
+  keyColIdx: number
+): { [key: string]: Row[] } {
+  const keyToRows = {};
+  for (let row of data) {
+    const key = row[keyColIdx];
+    keyToRows[key] = keyToRows[key] ?? [];
+    keyToRows[key].push(row);
+  }
+  return keyToRows;
+}
+
+export function uniqueValues(data: Row[], colIdx: number): string[] {
+  const values = new Set<string>();
+  for (let row of data) {
+    values.add(row[colIdx]);
+  }
+  return Array.from(values);
 }
