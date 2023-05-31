6daebcd711c679788eba1926e8bb710cf2ed7114
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-31 13:34:09 +0100

    Display typical charts for matched query pattern

--- frontend/src/components/query/Analysis.tsx
+++ frontend/src/components/query/Analysis.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
-import { Alert, Card, List, Space, Tag } from "antd";
+import { Alert, Card, List, Space, Tag, Tooltip, Typography } from "antd";
 import {
   ChartType,
   QueryAnalysis,
   RepositoryId,
   CategoryType,
   VariableCategories,
-  Visualisation,
 } from "../../types";
 import { getQueryAnalysis } from "../../api/queries";
 import { AiOutlineBarChart } from "react-icons/ai";
@@ -53,9 +52,9 @@ const Analysis = ({ query, repository }: AnalysisProps) => {
       {queryAnalysis && (
         <Space direction="vertical" style={{ width: "100%" }}>
           <Variables variableCategories={queryAnalysis.variables} />
-          {queryAnalysis.match ? (
-            <RecommendedCharts
-              query={query}
+          {queryAnalysis.pattern ? (
+            <Pattern
+              pattern={queryAnalysis.pattern}
               visualisations={queryAnalysis.visualisations}
             />
           ) : (
@@ -67,45 +66,24 @@ const Analysis = ({ query, repository }: AnalysisProps) => {
   );
 };
 
-type RecommendedProps = {
-  query: string;
-  visualisations: Visualisation[];
+type PatternProps = {
+  pattern: string;
+  visualisations: ChartType[];
 };
 
-const RecommendedCharts = ({ query, visualisations }: RecommendedProps) => {
+const Pattern = ({ pattern, visualisations }: PatternProps) => {
   return (
-    <Card
-      type="inner"
-      title={`Recommended chart${visualisations.length > 1 ? "s" : ""}`}
-      style={{ width: "100%" }}
-    >
-      <List
-        itemLayout="horizontal"
-        dataSource={visualisations}
-        renderItem={(item, index) => {
-          const { name, maxInstances } = item;
-          const ChartIcon = chartIcons[name];
-          return (
-            <List.Item>
-              <List.Item.Meta
-                key={name}
-                avatar={ChartIcon ? ChartIcon : name}
-                title={name}
-                description={
-                  <>
-                    {maxInstances && (
-                      <Alert
-                        banner
-                        message={`Add LIMIT ${maxInstances} in query for readability`}
-                      />
-                    )}
-                  </>
-                }
-              />
-            </List.Item>
-          );
-        }}
-      />
+    <Card type="inner" title={pattern} style={{ width: "100%" }}>
+      <Space direction="vertical">
+        <Typography.Text style={{ fontSize: 20 }}>
+          Typical charts
+        </Typography.Text>
+        <Space>
+          {visualisations.map((chart) => (
+            <Tooltip title={chart}>{chartIcons[chart] ?? chart}</Tooltip>
+          ))}
+        </Space>
+      </Space>
     </Card>
   );
 };
@@ -121,7 +99,7 @@ const categoryIcon = {
   [CategoryType.GEOGRAPHICAL]: <BsGeoAltFill title="Geographical" size={20} />,
   [CategoryType.SCALAR]: <MdNumbers title="Scalar" size={25} />,
   [CategoryType.LEXICAL]: <BiText title="Lexical" size={20} />,
-  [CategoryType.NUMERIC]: <Tb123 title="Numeric" size={25} />
+  [CategoryType.NUMERIC]: <Tb123 title="Numeric" size={25} />,
 };
 
 const Variables = ({ variableCategories }: VariablesProps) => {
