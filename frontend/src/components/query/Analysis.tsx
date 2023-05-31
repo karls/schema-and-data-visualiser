43cf043fb93e4747cda734764a76618e55e1d49b
Rohan Pandit <rohananandpandit@gmail.com>
2023-05-31 22:49:45 +0100

    Add remaining chart icons and add key for Tooltip

--- frontend/src/components/query/Analysis.tsx
+++ frontend/src/components/query/Analysis.tsx
@@ -8,19 +8,30 @@ import {
   VariableCategories,
 } from "../../types";
 import { getQueryAnalysis } from "../../api/queries";
-import { AiOutlineBarChart } from "react-icons/ai";
-import { BiScatterChart, BiText } from "react-icons/bi";
+import { AiOutlineBarChart, AiOutlineRadarChart } from "react-icons/ai";
+import { BiLineChart, BiScatterChart, BiText } from "react-icons/bi";
 import { VscGraphScatter } from "react-icons/vsc";
 import {
   BsBodyText,
   BsCalendar3,
   BsCalendarDateFill,
   BsGeoAltFill,
+  BsPieChart,
 } from "react-icons/bs";
 import { GoKey } from "react-icons/go";
-import { IoMdTime } from "react-icons/io";
-import { MdNumbers } from "react-icons/md";
-import { Tb123 } from "react-icons/tb";
+import { IoMdGitNetwork, IoMdTime } from "react-icons/io";
+import { MdNumbers, MdOutlineStackedBarChart } from "react-icons/md";
+import {
+  Tb123,
+  TbChartSankey,
+  TbChartTreemap,
+  TbCircles,
+  TbGridDots,
+} from "react-icons/tb";
+import { TiChartPieOutline } from "react-icons/ti";
+import { ImSphere, ImTree } from "react-icons/im";
+import { HiOutlineGlobe } from "react-icons/hi";
+import { RiBarChartGroupedFill } from "react-icons/ri";
 
 export const chartIcons = {
   [ChartType.BAR]: <AiOutlineBarChart size={35} />,
@@ -28,6 +39,20 @@ export const chartIcons = {
   [ChartType.BUBBLE]: <BiScatterChart size={35} />,
   [ChartType.WORD_CLOUD]: <BsBodyText size={30} />,
   [ChartType.CALENDAR]: <BsCalendar3 size={30} />,
+  [ChartType.PIE]: <BsPieChart size={30} />,
+  [ChartType.LINE]: <BiLineChart size={30} />,
+  [ChartType.TREE_MAP]: <TbChartTreemap size={30} />,
+  [ChartType.CIRCLE_PACKING]: <TbCircles size={30} />,
+  [ChartType.SUNBURST]: <TiChartPieOutline size={30} />,
+  [ChartType.SPIDER]: <AiOutlineRadarChart size={30} />,
+  [ChartType.SANKEY]: <TbChartSankey size={30} />,
+  [ChartType.CHORD_DIAGRAM]: <ImSphere size={30} />,
+  [ChartType.HEAT_MAP]: <TbGridDots size={30} />,
+  [ChartType.HIERARCHY_TREE]: <ImTree size={30} />,
+  [ChartType.NETWORK]: <IoMdGitNetwork size={30} />,
+  [ChartType.CHOROPLETH_MAP]: <HiOutlineGlobe size={30} />,
+  [ChartType.STACKED_BAR]: <MdOutlineStackedBarChart size={30} />,
+  [ChartType.GROUPED_BAR]: <RiBarChartGroupedFill size={30} />,
 };
 
 type AnalysisProps = {
@@ -80,7 +105,7 @@ const Pattern = ({ pattern, visualisations }: PatternProps) => {
         </Typography.Text>
         <Space>
           {visualisations.map((chart) => (
-            <Tooltip title={chart}>{chartIcons[chart] ?? chart}</Tooltip>
+            <Tooltip key={chart} title={chart}>{chartIcons[chart] ?? chart}</Tooltip>
           ))}
         </Space>
       </Space>
